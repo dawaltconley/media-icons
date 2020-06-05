72e5a997ac424efba82aba1e265dcde3df62947e
Dylan Awalt-Conley <web@dylan.ac>
2020-06-05 18:48:52 -0400

    added fa and passed through stylesheets to enable browser rendering

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,7 @@ const customIconTags = new MediaIcons().nunjucks;
 module.exports = eleventyConfig => {
     eleventyConfig.addDataExtension('yml', data => yaml.safeLoad(data))
 
+    eleventyConfig.addPassthroughCopy({ 'dist/icons.css': 'css/icons.css' });
     customIconTags(eleventyConfig);
 
     return {
