944b07e5d00dcf00fe92c4f4b2160001edfaba79
Dylan Awalt-Conley <web@dylan.ac>
2020-06-05 18:26:51 -0400

    added eleventy for testing

---
+++ .eleventy.js
@@ -0,0 +1,20 @@
+const yaml = require('js-yaml');
+const path = require('path');
+const p = (...args) => path.join(__dirname, ...args);
+const MediaIcons = require(p('tags.js'));
+
+const customIconTags = new MediaIcons().nunjucks;
+
+module.exports = eleventyConfig => {
+    eleventyConfig.addDataExtension('yml', data => yaml.safeLoad(data))
+
+    customIconTags(eleventyConfig);
+
+    return {
+        dir: {
+            input: './eleventy',
+            output: './eleventy/_site'
+        },
+        htmlTemplateEngine: 'njk'
+    }
+}
