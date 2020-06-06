2b9c47b853e0cc51ec4ff86fffc5aa5d9ae99b31
Dylan Awalt-Conley <web@dylan.ac>
2020-06-06 18:29:08 -0400

    remove dist before testing

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ const child = require('child_process');
 const fs = require('fs');
 const assert = require('assert').strict;
 
+fs.rmdirSync('./dist', { recursive: true });
+
 child.exec('node build; npx eleventy', (err) => {
     if (err) throw err;
     const result = fs.readFileSync('./eleventy/_site/index.html').toString();
